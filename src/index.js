38770901b27195694fecce5456a841607beada04
arshia <arshia.karimi@gmail.com>
2024-05-24 17:30:45 +0330

    end

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider, createTheme } from "@mui/material";
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#010660",
+      contrastText: "#FFF",
+    },
+  },
+  typography: { fontFamily: "dana" },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
